17114560d2d17ba687a4badbee5fdecaba2d6320
Benjamin Gallo <benjamin221999@gmail.com>
2023-10-01 17:10:59 -0300

    agrego componente

--- tp2-interfaces/src/App.js
+++ tp2-interfaces/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar_c from './components/Navbar_c';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Navbar_c />
     </div>
   );
 }
---
+++ tp2-interfaces/src/components/Navbar_c.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar_c() {
+    return (
+        <div>
+            <div>HOLA BENJA</div>
+            <div>ESTOY FUNCIONANDO</div>
+        </div>
+    );
+}
+
+export default Navbar_c;
